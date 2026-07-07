048026b1daec90d276918052da45b17a7a0a8b7d
Ashish rai <152901055+aashishraieng@users.noreply.github.com>
2026-07-07 11:09:55 +0530

    Time: 1 ms (99.94%), Space: 53 MB (12.46%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,45 +11,62 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int la=0;
-        int lb=0;
-        ListNode temA=headA;
-        ListNode temB=headB;
-        while(temA!=null){
-            la++;
-            temA=temA.next;           
+        // int la=0;
+        // int lb=0;
+        // ListNode temA=headA;
+        // ListNode temB=headB;
+        // while(temA!=null){
+        //     la++;
+        //     temA=temA.next;           
 
-        }
-        while(temB!=null){
-            lb++;
-            temB=temB.next;           
+        // }
+        // while(temB!=null){
+        //     lb++;
+        //     temB=temB.next;           
 
-        }
-        temA=headA;
-        temB=headB;
+        // }
+        // temA=headA;
+        // temB=headB;
 
-        if(lb>la){
-            int d=lb-la;
-            for(int i=0;i<d;i++){
-                temB=temB.next;
+        // if(lb>la){
+        //     int d=lb-la;
+        //     for(int i=0;i<d;i++){
+        //         temB=temB.next;
+        //     }
+        // }else{
+        //     int d=la-lb;
+        //     for(int i=0;i<d;i++){
+        //         temA=temA.next;
+        //     }
+
+        // }
+
+        // while(temA!=null){
+        //     if(temA==temB){
+        //         return temA;
+        //     }
+        //     temA=temA.next;
+        //     temB=temB.next;
+        // }
+
+        // return null;
+
+        ListNode tempA=headA;
+        ListNode tempB=headB;
+        while(tempA!=tempB){
+            if(tempA==null){
+                tempA=headB;
+            }else{
+                tempA=tempA.next;
             }
-        }else{
-            int d=la-lb;
-            for(int i=0;i<d;i++){
-                temA=temA.next;
+            if(tempB==null){
+                tempB=headA;
+            }else{
+                tempB=tempB.next;
             }
-
         }
 
-        while(temA!=null){
-            if(temA==temB){
-                return temA;
-            }
-            temA=temA.next;
-            temB=temB.next;
-        }
-
-        return null;
+        return tempA;
         
     }
 }
